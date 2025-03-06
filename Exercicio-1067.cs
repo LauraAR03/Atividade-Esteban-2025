9afbc73668eaffe3caf37a510a74b192f166608d
laura <laura@LAURA_AFONSO03>
2025-03-06 12:38:07 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio-1067.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Exercicio1067
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("escreva um numero inteiro: ");
+        int X = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i<= X; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+}
